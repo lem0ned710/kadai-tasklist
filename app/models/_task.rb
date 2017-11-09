ec48e283456e6a0e8db357ac75008d73c84170fb
daishi.tenjimbayashi <lem0n.squeeeeez@gmail.com>
2017-11-09 15:01:36 +0000

    bugfix

--- app/models/_task.rb
+++
@@ -1,7 +0,0 @@
-class Task < ApplicationRecord
-  belongs_to :user_id
-  
-  validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 255 }
-  validates :status, presence: true, length: { maximum: 255 }
-end
